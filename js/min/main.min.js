0f0f3166d7183adf725d345e74e42f800145417f
oraricha <orarich@gmail.com>
2014-02-06 02:18:40 +0200

    Added correct bg images Added about us section Changed next button behavior Fixed logic of links Added correct links to site header

--- js/min/main.min.js
+++ js/min/main.min.js
@@ -1,15 +1,41 @@
 $window = $(window);
 
-$(document).ready(function(){
+$(document).ready(function() {
+
+	// methods
+	var resizeHeader = function() {
+		$window.on("scroll", function() {
+			var fromTop = $("body").scrollTop();
+			$(".site-header").toggleClass("shrink", (fromTop > 200));
+		});
+	};
+
+	var setSectionHeight = function() {
+		var viewportHeight = $window.height();
+		var sections = $("body section");
+		sections.each(function(index, element) {
+			element = $(element);
+			if (element.height() < viewportHeight) {
+				element.height(viewportHeight);
+			}
+		});
+	};
+
+	var setSliderHeight = function () {
+		var slider = $(".about .slider");
+		var summary = $(".summary");
+		slider.height(summary.height() + 10 + "px");
+	};
+
+	// logic
 	setSectionHeight();
 	setSliderHeight();
 
-	$(".next").click(function(e) {
+	$(".next, .site-header a").click(function(e) {
 		e.preventDefault();
 		var href = e.target.href;
 		var target = href.indexOf("#");
 		target = href.substr(target);
-		console.log(target);
 		$("html, body").animate({
 			scrollTop: $(target).offset().top + 1
 		}, 1500);
@@ -23,20 +49,4 @@ $(document).ready(function(){
 	});
 });
 
-var resizeHeader = function() {
-	$window.on("scroll", function() {
-		var fromTop = $("body").scrollTop();
-		$(".site-header").toggleClass("shrink", (fromTop > 200));
-	});
-};
 
-var setSectionHeight = function() {
-	var viewportHeight = $window.height();
-	$("body section").height(viewportHeight);
-};
-
-var setSliderHeight = function () {
-	var slider = $(".about .slider");
-	var summary = $(".summary");
-	slider.height(summary.height() + 10 + "px");
-};
