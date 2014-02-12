e3d6f5a8e82af888c69c3cca9ae48ae29a592ff8
oraricha <orarich@gmail.com>
2014-02-12 02:53:35 +0200

    Added contact form + validation

--- js/min/main.min.js
+++ js/min/main.min.js
@@ -32,8 +32,9 @@ $(document).ready(function() {
 	setSectionHeight();
 	setSliderHeight();
 
-	$(".next, .site-header a").click(function(e) {
+	$(".next a, .site-header a").click(function(e) {
 		e.preventDefault();
+		e.stopPropagation();
 		var href = e.target.href;
 		var target = href.indexOf("#");
 		target = href.substr(target);
@@ -52,6 +53,71 @@ $(document).ready(function() {
 		setSectionHeight();
 		setSliderHeight();
 	});
+
+	var formActions = (function() {
+		var form = $(".contact form");
+		var submitButton = form.find("button[type=submit]");
+
+		// submitButton.click(function(e){
+		// 	e.preventDefault();
+		// });
+
+		var validateForm = function(inputs) {
+			var isValidationPassed = true;
+			// var inputs = $.makeArray(inputs);
+			for (var i = 0; i < inputs.length; i++) {
+				var currentInput = inputs[i];
+				var currentValue = currentInput.val();
+				if (currentValue === "") {
+					isValidationPassed = false;
+					currentInput.addClass("error");
+					continue;
+				}
+
+				if (currentInput.attr("id") === "email") {
+					var emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+					isValidationPassed = emailRegex.test(currentValue);
+				}
+
+				if (isValidationPassed) {
+					currentInput.removeClass("error");
+				}
+			};
+
+			return isValidationPassed;
+		};
+
+		form.submit(function(e){
+			e.preventDefault();
+			console.log('submitted');
+
+			// var inputs = {
+			// 	name: form.find("input[id=name]"),
+			// 	email: form.find("input[id=email]"),
+			// 	subject: form.find("input[id=subject]")
+			// };
+
+			var inputs = [];
+			inputs.push(form.find("input[id=name]"));
+			inputs.push(form.find("input[id=email]"));
+			inputs.push(form.find("textarea[id=subject]"));
+
+			if (validateForm(inputs)) {
+				var request = {
+					url: "sendMail.php",
+					data: form.serialize(),
+				};
+				console.log(request.data);
+				$.ajax({
+					type: "POST",
+					url: request.url,
+					data: request.data/*,
+					success: success,
+					dataType: dataType*/
+				});
+			}
+		});
+	})();
 });
 
 
