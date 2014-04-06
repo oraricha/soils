596e34a3d5d25fb07d14196e519b7c7e85d70f62
oraricha <orarich@gmail.com>
2014-04-07 02:22:48 +0300

    Removed all comments Minified & uglified js

--- server.php
+++ server.php
@@ -3,7 +3,7 @@
 	//if "email" is filled out, send email
 		//send email
 		$email = $_REQUEST['email'];
-		$to = "a@a.com, a@a.com";
+		$to = "uxappdev@gmail.com";
 		$name = $_REQUEST['name'];
 		$subject = "Message from sensoils.com (From:" . $name . "(" . $email . ")";
 		$message = $_REQUEST['message'];
